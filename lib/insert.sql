2d52a9dd513ad119da6a55e98d252aacf430714b
Jenise Ruehle <jela07@gmail.com>
2020-07-24 16:10:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "chocolate brown", "sweet", 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "striped", "rambunctious", 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "satin brown", "very nice", 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
